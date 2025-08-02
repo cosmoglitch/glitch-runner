ff21746e66866de0de9cf03ae268784e2626daeb
Svar <emailsanchi@gmail.com>
2025-08-03 00:16:23 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/glitch_runner_animated_transparent.gif",
       navbar: {
         title: "Glitch-Runner",
         logo: {
