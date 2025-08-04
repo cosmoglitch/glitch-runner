61a8661e782afa6ee4b8bed9a5246312d7e30181
Svar <emailsanchi@gmail.com>
2025-08-04 21:31:47 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,6 +10,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+  
   title: "Glitch-Runner",
   tagline: "",
   //favicon: "img/favicon.ico",
